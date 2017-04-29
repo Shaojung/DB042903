8dd5b56cd464dfa2786026c3dd675f9690352e1c
Shaojung <shaojung@gmail.com>
2017-04-29 13:46:11 +0800

    Show Time

--- app/src/main/java/com/example/teacher/db042903/MyService.java
+++ app/src/main/java/com/example/teacher/db042903/MyService.java
@@ -2,13 +2,25 @@ package com.example.teacher.db042903;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.IBinder;
 import android.support.annotation.IntDef;
 import android.util.Log;
 
 public class MyService extends Service {
+    Handler handler = new Handler();
+
     public MyService() {
     }
+
+    Runnable showTime = new Runnable() {
+        @Override
+        public void run() {
+            Log.d("SER", "Time:" + new java.util.Date());
+            handler.postDelayed(this, 1000);
+        }
+    };
+
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -27,6 +39,7 @@ public class MyService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d("SER", "on Start Command");
+        handler.post(showTime);
         return super.onStartCommand(intent, flags, startId);
     }
 }
